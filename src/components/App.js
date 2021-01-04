fbe4ce4fefbc4a52bed5bfa18016cdf370d5009b
Gaurav-dawadi <gaurabdawadi@gamil.com>
2021-01-05 01:56:14 +0545

    feat: add  components folder and create new App component

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import TodoBox from './todoBox'
+import './App.css'
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className='main-wrapper'>
+                <div className='title-wrapper'>
+                    <h1>ToDo List</h1>
+                </div>
+                <div className='component-wrapper'>
+                    <TodoBox/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App
